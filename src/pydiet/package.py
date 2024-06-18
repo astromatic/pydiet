d44050698aea729fe4c8623514e1e83531efaec7
Emmanuel Bertin <bertin@iap.fr>
2024-06-18 14:41:05 +0200

    Re-factor package files.

--- src/pydiet/package.py
+++ src/pydiet/package.py
@@ -4,40 +4,47 @@ Package-wide definitions.
 # Copyright CEA/CFHT/CNRS/UParisSaclay
 # Licensed under the MIT license
 
+from re import match
 from os import path
 from pathlib import Path
 from importlib.metadata import metadata
+from importlib.resources import files
 import sys
 
 import platformdirs
 
 # Package information
 # Get the current package name, quick and (very) dirty
-name = __name__.split(".")[-2] #: 
-title = "PyDIET" #: 
-meta = metadata(name) #: 
-version = meta["Version"] #: 
-summary = meta["Summary"] #: 
-description = meta["Description"] #: 
-url = meta["Project-URL"].rsplit(", ")[-1] #:
+name = __name__.split(".")[-2]
+meta = metadata(name)
+title = meta['Name']
+version = meta['Version']
+summary = meta['Summary']
+description = meta['Description']
+url = meta['Project-URL'].rsplit(", ")[-1]
 
-# Contact information
-contact_name = "Emmanuel Bertin" #: 
-contact_affiliation = "CEA/CFHT/CNRS/UParisSaclay" #: 
-contact_email = "emmanuel.bertin@universite-paris-saclay.fr" #: 
+# Contact
+authors = meta['Author-email'].split(", ")
+if authors:
+    contact = match(
+        r'\"?\s*(?P<name>[A-Za-zÀ-ÖØ-öø-ÿ\s]*?)\s*(?:\(\s*'
+        '(?P<affiliation>[^)]+?)\s*\))?\"?\s*<\s*(?P<email>[^>]+?)\s*>?\s*$',
+        authors[0]
+    ).groupdict()
 
 # License
-license_name = "MIT" #: 
-license_url = "https://spdx.org/licenses/MIT.html" #: 
+#license = meta['License']
+license_name = "MIT"
+license_url = "https://spdx.org/licenses/MIT.html"
 
-#: Package source directory
-src_dir = path.dirname(path.abspath(__file__)) 
+# Package source directory
+src_dir = path.dirname(path.abspath(__file__))
 
-#: Package root directory
-root_dir = Path(src_dir).parent.parent
+# Package root directory
+root_dir = Path(str(files(name)))
 
-#: Default configuration file
-config_file = path.join(platformdirs.user_config_dir(name), "pydiet.conf")
+# Default configuration file
+config_file = path.join(platformdirs.user_config_dir(name), f"{name}.conf")
 
 # Default cache dir
 cache_dir = platformdirs.user_cache_dir(name)
