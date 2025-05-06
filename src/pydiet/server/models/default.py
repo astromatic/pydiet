aba357d0a0db6db80e8c98190cb68b0bc6eea097
Emmanuel Bertin <bertin@iap.fr>
2025-05-06 16:08:30 +0200

    Add missing file.

---
+++ src/pydiet/server/models/default.py
@@ -0,0 +1,9 @@
+"""
+Default entries for models
+"""
+# Copyright CFHT/CNRS/CEA/UParisSaclay
+# Licensed under the MIT licence
+
+# We actually gather default entities from the data module
+from ..data import default_filter, default_instrument, filters, instruments
+
