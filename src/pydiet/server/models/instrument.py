87f24bbfa5853fa4e151a1ef6b16e81854c7a844
Emmanuel Bertin <bertin@iap.fr>
2025-06-24 12:56:29 +0200

    Refactor slightly and fix mypy issues..

--- src/pydiet/server/models/instrument.py
+++ src/pydiet/server/models/instrument.py
@@ -14,28 +14,45 @@ from ... import package
 from ..types import AnnotatedQuantity
 
 
-class SEDModel(BaseModel):
+class DetectorModel(BaseModel):
     '''
-    Pydantic model for a Spectral Energy Distribution (SED).
+    Pydantic model for an instrument detector (e.g., CMOS or CCD).
+    '''
+    gain: AnnotatedQuantity(    #type: ignore[valid-type]
+        unit = "electron / adu",
+        gt = 0. * u.electron / u.adu,
+        decimals = 4
+    )
+    ron: AnnotatedQuantity(    #type: ignore[valid-type]
+        unit = "electron",
+        ge = 0. * u.electron,
+        decimals = 4
+    )
+    scale: AnnotatedQuantity(    #type: ignore[valid-type]
+        unit = "arcsec/pix",
+        gt = [0., 0.] * u.arcsec / u.pix,
+        min_shape = (2),
+        max_shape = (2),
+        decimals = 4,
+        description = "Angular pixel scale along each axis."
+    )
+    qes: Dict[str, 'TransmissionModel']
+
+
+
+class InstrumentModel(BaseModel):
+    '''
+    Pydantic model for a PyDIET instrument.
     '''
     id: str
     name: str
     description: str
-    wave: AnnotatedQuantity(    #type: ignore[valid-type]
-        unit = "nm",
-        ge = 100. * u.nm,
-        le = 100. * u.micron,
-        min_shape = (2),
-        max_shape = (20000),
-        decimals = 4
-    )
-    sed:  AnnotatedQuantity(    #type: ignore[valid-type]
-        unit = "Jy",
-        ge = 0. * u.Jy,
-        min_shape = (2),
-        max_shape = (20000),
-        decimals = 6
-    )
+    filters: Dict[str, 'TransmissionModel']
+    optics: Dict[str, 'TransmissionModel']
+    detector: 'DetectorModel'
+    telescope: 'TelescopeModel'
+    site: 'SiteModel'
+    default: bool = False
 
 
 
@@ -67,6 +84,72 @@ class SBSEDModel(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
+
+class SEDModel(BaseModel):
+    '''
+    Pydantic model for a Spectral Energy Distribution (SED).
+    '''
+    id: str
+    name: str
+    description: str
+    wave: AnnotatedQuantity(    #type: ignore[valid-type]
+        unit = "nm",
+        ge = 100. * u.nm,
+        le = 100. * u.micron,
+        min_shape = (2),
+        max_shape = (20000),
+        decimals = 4
+    )
+    sed:  AnnotatedQuantity(    #type: ignore[valid-type]
+        unit = "Jy",
+        ge = 0. * u.Jy,
+        min_shape = (2),
+        max_shape = (20000),
+        decimals = 6
+    )
+    spectral: SourceSpectrum = Field(exclude=True)
+    default: bool = False
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+
+class SiteModel(BaseModel):
+    '''
+    Pydantic model for an observing site.
+    '''
+    id: str
+    name: str
+    description: str
+    sky_transmissions: Dict[str, 'TransmissionModel']
+    sky_emissions: Dict[str, 'SBSEDModel']
+    default: bool = False
+
+
+
+class TelescopeModel(BaseModel):
+    '''
+    Pydantic model for a telescope.
+    '''
+    id: str
+    name: str
+    description: str
+    collecting_area: AnnotatedQuantity(    #type: ignore[valid-type]
+        unit = "m**2",
+        gt = 0. * u.m**2,
+        decimals = 4
+    )
+    obstruction_area: AnnotatedQuantity(    #type: ignore[valid-type]
+        unit = "m**2",
+        gt = 0. * u.m**2,
+        decimals = 4
+    )
+    transmissions: Dict[str, 'TransmissionModel']
+    emissions: Dict[str, 'SBSEDModel']
+    default: bool = False
+
+
+
 class TransmissionModel(BaseModel):
     '''
     Pydantic model for a transmission curve (with wavelength).
@@ -94,82 +177,5 @@ class TransmissionModel(BaseModel):
     default: bool = False
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
-
-
-class DetectorModel(BaseModel):
-    '''
-    Pydantic model for an instrument detector (e.g., CMOS or CCD).
-    '''
-    gain: AnnotatedQuantity(    #type: ignore[valid-type]
-        unit = "electron / adu",
-        gt = 0. * u.electron / u.adu,
-        decimals = 4
-    )
-    ron: AnnotatedQuantity(    #type: ignore[valid-type]
-        unit = "electron",
-        ge = 0. * u.electron,
-        decimals = 4
-    )
-    scale: AnnotatedQuantity(    #type: ignore[valid-type]
-        unit = "arcsec/pix",
-        gt = [0., 0.] * u.arcsec / u.pix,
-        min_shape = (2),
-        max_shape = (2),
-        decimals = 4,
-        description = "Angular pixel scale along each axis."
-    )
-    qes: Dict[str, 'TransmissionModel']
-
-
-
-class TelescopeModel(BaseModel):
-    '''
-    Pydantic model for the telescope.
-    '''
-    id: str
-    name: str
-    description: str
-    collecting_area: AnnotatedQuantity(    #type: ignore[valid-type]
-        unit = "m**2",
-        gt = 0. * u.m**2,
-        decimals = 4
-    )
-    obstruction_area: AnnotatedQuantity(    #type: ignore[valid-type]
-        unit = "m**2",
-        gt = 0. * u.m**2,
-        decimals = 4
-    )
-    transmissions: Dict[str, 'TransmissionModel']
-    emissions: Dict[str, 'SBSEDModel']
-    default: bool = False
-
-
-
-class SiteModel(BaseModel):
-    '''
-    Pydantic model for the observing site.
-    '''
-    id: str
-    name: str
-    description: str
-    sky_transmissions: Dict[str, 'TransmissionModel']
-    sky_emissions: Dict[str, 'SBSEDModel']
-    default: bool = False
-
-
-
-class InstrumentModel(BaseModel):
-    '''
-    Pydantic model for a PyDIET instrument.
-    '''
-    id: str
-    name: str
-    description: str
-    filters: Dict[str, 'TransmissionModel']
-    optics: Dict[str, 'TransmissionModel']
-    detector: DetectorModel
-    telescope: TelescopeModel
-    site: SiteModel
-    default: bool = False
 
 
