7c36be6f91e2aedbfce8a5b913916cdb5f9fe47c
Conrad Holmberg <holmberg@cfht.hawaii.edu>
2026-02-26 15:23:53 -1000

    Added docker compose file and modified entrypoint for running.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,5 +4,5 @@ set -e
 # Activate the virtual environment
 source venv_pydiet/bin/activate
 
-# Execute the main command passed to the container (e.g., "python your_app_script.py")
+# Execute the Docker fun command passed to the container (e.g., "pydiet --host 0.0.0.0")
 exec "$@"
